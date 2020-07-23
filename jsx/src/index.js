d7387bb984d380096404681ab5fee9f538f18a53
elise navarro <elise.marie.navarro@gmail.com>
2020-07-23 13:57:48 -0500

    Completed Components

---
+++ jsx/src/index.js
@@ -0,0 +1,24 @@
+// Import the React and ReactDOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+    const buttonText = { text: 'Click me' };
+    const labelText = 'Enter name:';
+
+    return (
+      <div>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>
+            {buttonText.text}
+        </button>
+      </div>
+    )
+};
+
+// Take the react component and display on screen
+ReactDOM.render(<App />, document.querySelector('#root'));
