708f7e87acfdcf587ae9a28b96cdf1bb56122e78
elise navarro <elise.marie.navarro@gmail.com>
2020-08-16 14:26:00 -0500

    Set Route Path + Global Nav

--- streaming/client/src/components/App.js
+++ streaming/client/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamUpdate from './streams/StreamUpdate';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
-  return <div>App</div>
-}
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/update' exact component={StreamUpdate} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
+        </div>
+      </BrowserRouter> 
+    </div>
+  );
+};
 
 export default App;
---
+++ streaming/client/src/components/streams/StreamUpdate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamUpdate  = () => {
+  return <div> StreamUpdate</div>;
+};
+
+export default StreamUpdate;
