70d1e645854664bf4018ef70646d0f5f9dbf027a
elise navarro <elise.marie.navarro@gmail.com>
2020-09-12 14:44:08 -0500

    Simple toggle app using Context

---
+++ translate/src/components/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
+}
+
+export default App;
